3a0b83c98c8eb772802505d6de7020dd1e909d3e
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-10 03:14:10 +0530

    Looped texts

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//Sportsman, Student, VITian, Musician, Scuba Diver
+//Wait till the DOM is ready.
+$(function () {
+  var roles = ["Student", "VITian", "Sportsman", "Musician", "Scuba Diver"];
+  var count = 0;
+  setInterval(() => {
+    console.log(roles[count++ % roles.length]);   
+  },  1000);
+});
