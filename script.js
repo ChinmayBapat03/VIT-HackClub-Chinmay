57a1f25b1faf1976b73a25fe893e8607db0f0b66
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-10 11:07:17 +0530

    Addded links to texts

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //Wait till the DOM is ready.
 $(function () {
   var roles = ["Student", "VITian", "Sportsman", "Musician", "Scuba Diver"];
+  var urls = ["","","","","https://www.youtube.com/watch?v=Ix5gE_llx-Q"]
   var count = 0;
   setInterval(() => {
-   $("#intro .content h2 span").fadeOut("fast", function() {
-     $(this).text(roles[count++ % roles.length]);
-     $(this).fadeIn("fast");   
+   $("#intro .content h2 span").fadeOut("slow", function() {
+     $(this).text(roles[count]);
+     $(this).fadeIn("slow");   
+     $('#skill_url').attr("href", urls[count++ % roles.length]);
    });   
-  },  1000);
+  },  2500);
 });
