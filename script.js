d84cbb82fbd456af5244945cebb99e0e2c00088d
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-10 11:21:02 +0530

    bruh

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(function () {
   var urls = ["","","","","https://www.youtube.com/watch?v=Ix5gE_llx-Q"]
   var count = 0;
   setInterval(() => {
-    count = 0;
+    //count = 0;
    $("#intro .content h2 span").fadeOut("slow", function() {
-     $(this).text(roles[count]);
+     $(this).text(roles[count++ % roles.length]);
      $(this).fadeIn("slow");   
-     $('#skill_url').attr("href", urls[count++ % roles.length]);
+     //$('#skill_url').attr("href", urls[count++ % roles.length]);
    });   
   },  1500);
 });
