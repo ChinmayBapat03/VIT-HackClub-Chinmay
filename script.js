9b7b5cce151f024f9cd94bdd67a290d096ab0ca6
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-10 11:26:19 +0530

    added url count

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(function () {
   var roles = ["Student", "VITian", "Sportsman", "Musician", "Scuba Diver"];
   var urls = ["","","","","https://www.youtube.com/watch?v=Ix5gE_llx-Q"]
   var count = 0;
+  var url_count = 0;
   setInterval(() => {
     //count = 0;
    $("#intro .content h2 span").fadeOut("slow", function() {
