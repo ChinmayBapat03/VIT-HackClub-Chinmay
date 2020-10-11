b74f19c3bc3a6c8fe05cdee6a008c4ea60c3430a
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-11 21:38:03 +0530

    Made nav bar to go down after scrolled up.

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ $(function () {
    });   
   },  1500);
 });
+  $(".navbar").hide();
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 40) {
+      $(".navbar").slideDown(100);
+    } else {
+      $(".navbar").slideUp(100);
+    }
+  });
