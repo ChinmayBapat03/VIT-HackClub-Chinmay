5683726ae74c875892940bf7838582045e865109
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-11 22:29:01 +0530

    Added type effect

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ $(function () {
   var urls = ["","","","","https://www.youtube.com/watch?v=Ix5gE_llx-Q"]
   var count = 0;
   var url_count = 0;
+  var $typeSpan = $("#intro .content h2 span").typist({
+    text: roles[count]
+  });   
   setInterval(() => {
     //count = 0;
-   $("#intro .content h2 span").fadeOut("slow", function() {
-     $(this).text(roles[count++ % roles.length]);
-     $(this).fadeIn("slow");   
+   //$("#intro .content h2 span").fadeOut("slow", function() {
+     //$(this).text(roles[count++ % roles.length]);
+     //$(this).fadeIn("slow");   
      $('#skill_url').attr("href", urls[url_count++ % roles.length]);
-   });   
-  },  1500);
-});
+   });
+   
+   $typeSpan.typistRemove(roles[count++].length).typistPause(2000).typistAdd(roles[count]);
+  },  5000);
   $(".navbar").hide();
   $(window).scroll(function() {
     if ($(window).scrollTop() > 40) {
