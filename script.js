f5c04c3d6d9829f0ad32902f1eae17c5f8ec8c60
Chinmay Bapat <chinmay.bapat2020@vit.ac.in>
2020-10-12 22:51:49 +0530

    adjusted indentation

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 //Wait till the DOM is ready.
 $(function () {
   var roles = ["Student", "VITian", "Sportsman", "Musician", "Scuba Diver"];
-  var urls = ["","","","","https://www.youtube.com/watch?v=Ix5gE_llx-Q"]
+  var urls = ["","","","","https://www.youtube.com/watch?v=Ix5gE_llx-Q"];
   var count = 0;
   var url_count = 0;
   var $typeSpan = $("#intro .content h2 span").typist({
     text: roles[count]
   });   
-  setInterval(() => {
-    count = 0;
-  $("#intro .content h2 span").fadeOut("slow", function() {
-    $(this).text(roles[count++ % roles.length]);
-    $(this).fadeIn("slow");   
-    $('#skill_url').attr("href", urls[url_count++ % roles.length]);
-  },1000);
-   
-   //$typeSpan
-     //.typistRemove(roles[count++ % roles.length].length)
-     //.typistPause(2000)
-     //.typistAdd(roles[count % roles.length]);
-  //}, 5000);
-  $(".navbar").hide();
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 40) {
-      $(".navbar").slideDown(100);
-    } else {
-      $(".navbar").slideUp(100);
+  
+  setInterval
+  (() => 
+    {
+      count = 0;
+      $("#intro .content h2 span").fadeOut("slow", function() 
+        {
+          $(this).text(roles[count++ % roles.length]);
+          $(this).fadeIn("slow");   
+          $('#skill_url').attr("href", urls[url_count++ % roles.length]);
+        },1000);
     }
-  });
+  );
+   /*
+   $typeSpan
+     .typistRemove(roles[count++ % roles.length].length)
+     .typistPause(2000)
+     .typistAdd(roles[count % roles.length]);
+  }, 5000);
+  */
+    $(".navbar").hide();
+    $(window).scroll
+    (
+      function () 
+      {
+        if ($(window).scrollTop() > 40) 
+        {
+          $(".navbar").slideDown(100);
+        } 
+        else 
+        {
+          $(".navbar").slideUp(100);
+        }
+      }
+    );
+});
